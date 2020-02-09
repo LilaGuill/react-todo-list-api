4cfee22eb6ddc76daaceecc12caa1a88c64c6888
Lila Guillermic <lila.guillermic@gmail.com>
2020-02-09 17:11:51 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const middlewareFormidable = require("express-formidable");
+
+app.use(middlewareFormidable());
+app.use(cors());
+require("dotenv").config();
+
+mongoose.connect("process.env.MONGODB_URI", {
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+});
+
+const Todo = mongoose.model("Todo", {
+  task: {
+    type: Object
+  }
+});
+
+app.post("/create", async (req, res) => {
+  await Todo.remove();
+
+  const newTodo = new Todo({
+    task: req.fields.task
+  });
+  await newTodo.save();
+
+  res.send(newTodo);
+  try {
+  } catch (error) {
+    res.send(error.message);
+  }
+});
+
+app.listen(process.env.PORT, (req, res) => {
+  console.log("server started");
+});
