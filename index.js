ddd5f90039e3315781d0dc26a21216417c7937ff
Lila Guillermic <lila.guillermic@gmail.com>
2020-02-16 11:54:07 +0100

    creation de la route update et remove

--- index.js
+++ index.js
@@ -16,21 +16,14 @@ mongoose.connect(process.env.MONGODB_URI, {
 
 const Todo = mongoose.model("Todo", {
   task: {
-    type: String
+    type: String,
+    tolowercase: true
   },
   isChecked: {
     type: Boolean
   }
 });
-
-app.get("/", async (req, res) => {
-  try {
-    const todos = await Todo.find();
-    res.json(todos);
-  } catch (error) {
-    res.json({ message: error.message });
-  }
-});
+//Create
 app.post("/create", async (req, res) => {
   const newTodo = await new Todo({
     task: req.fields.task,
@@ -39,10 +32,46 @@ app.post("/create", async (req, res) => {
 
   await newTodo.save();
 
-  res.json(newTodo);
+  res.json({ message: "task created" });
   try {
   } catch (error) {
     res.json(error.message);
+  }
+});
+
+//read
+app.get("/", async (req, res) => {
+  try {
+    const todos = await Todo.find();
+    res.json(todos);
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+});
+
+//update
+app.post("/update", async (req, res) => {
+  try {
+    const todoToUpdate = await Todo.findById(req.fields.id);
+
+    todoToUpdate.isChecked = !todoToUpdate.isChecked;
+
+    await todoToUpdate.save();
+    res.json(todoToUpdate);
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+});
+
+// delete
+app.post("/remove", async (req, res) => {
+  try {
+    const todoToRemove = await Todo.findById(req.fields.id);
+    await todoToRemove.remove();
+
+    res.status(200).json({ message: "task removed" });
+  } catch (error) {
+    res.json({ message: error.message });
   }
 });
 
